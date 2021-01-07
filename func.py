77a4e99f0de9f78b3c4a6b0ee5ee5beba838e527
Clarence.B.Praison <71861162+clarencepraison@users.noreply.github.com>
2021-01-07 16:32:32 +0530

    Add files via upload
    login user and password using if

---
+++ func.py
@@ -0,0 +1,11 @@
+x=["ak","bk","lk"]
+y=["a","b","c"]
+user_name=input("Enter name:\n")
+if user_name in x:
+    password=input("enter password:\n")
+    if password in y:
+        print("Login Successfully")
+    else:
+        print("enter a vaild password")
+else:
+    print("enter a vaild username")
