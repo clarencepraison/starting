b4130f3f7ba2f2c51a2db30a582d95fc74f686ff
Clarence.B.Praison <71861162+clarencepraison@users.noreply.github.com>
2021-01-09 19:02:13 +0530

    Add files via upload
    basic concept of login using while.

---
+++ while_.py
@@ -0,0 +1,12 @@
+n=input("enter name:\n")
+name=['ak','bk','lk']
+password=['a','b','c']
+while n not in name:
+  print("enter a valid name")
+  n=input()
+print("thankyou")
+m=input("enter password:\n")
+while m not in password:
+  print("enter a vaild password")
+  m=input()
+print("valid")
