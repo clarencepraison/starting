ac4c4c61aec30b19de151ab9ab967a6c306d64ef
Clarence.B.Praison <71861162+clarencepraison@users.noreply.github.com>
2021-04-19 14:48:14 +0530

    Update assert.py

--- assert.py
+++ assert.py
@@ -1,61 +1,27 @@
 import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow,QApplication
 
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QHBoxLayout
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QLineEdit
-from PyQt5.QtWidgets import QFormLayout
-from PyQt5.QtWidgets import QDialogButtonBox
-
-
-
-class Assert(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
-        
-        super().__init__()
-
-        self.setWindowTitle("IT Assert")
-        self.setFixedSize(1420,900)
-        self.generalLayout=QHBoxLayout()
-        
-        self._centralWidget=QWidget(self)
-        self.setCentralWidget(self._centralWidget)
-        self._centralWidget.setLayout(self.generalLayout)
-
-        self._createDisplay()
+        super(MainWindow,self).__init__()
+        loadUi("main_assert.ui",self)
+        self.tableWidget.setColumnWidth(0, 200)
+        self.tableWidget.setColumnWidth(1, 205)
+        self.tableWidget.setColumnWidth(2, 125)
+        self.tableWidget.setColumnWidth(3, 205)
         
 
-    def _createDisplay(self):
 
-        dlgLayout = QHBoxLayout()
-        formLayout= QFormLayout()
-        formLayout.addRow('Name:',QLineEdit())
-        formLayout.addRow('Serial.No:',QLineEdit())
-        formLayout.addRow('Date:',QLineEdit())
-        formLayout.addRow('Vendor Name:',QLineEdit())
-        dlgLayout.addLayout(formLayout)
-        btns=QDialogButtonBox()
-        btns.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
-        dlgLayout.addWidget(btns)
-        self.setLayout(dlgLayout)
-        self.generalLayout.addLayout(dlgLayout)
-        self.generalLayout.addLayout(formLayout)
-        
-        
-        
-        
-
-       
-
-
-def main():
-
-    a=QApplication(sys.argv)
-    view=Assert()
-    view.show()
-    sys.exit(a.exec_())
-
-if __name__ == '__main__':
-    main()
+app=QApplication(sys.argv)
+mainwindow=MainWindow()
+widget=QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedHeight(700)
+widget.setFixedWidth(800)
+widget.show()
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting")
