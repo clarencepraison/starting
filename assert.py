fcbdaa2179a161011ae341309eba35ba05f84c17
Clarence.B.Praison <71861162+clarencepraison@users.noreply.github.com>
2021-05-02 03:06:17 +0530

    Update assert.py

--- assert.py
+++ assert.py
@@ -1,27 +1,160 @@
 import sys
+import pyodbc
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow,QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QMessageBox, QWidget, QTableWidgetItem,QLineEdit
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtCore import Qt, QSortFilterProxyModel
 
-class MainWindow(QMainWindow):
+server = '10.209.25.201'
+database = 'AssertDB'
+username = 'sa'
+password = 'devctti@123'
+
+
+class query:
+    def query_s(self):
+        self.tableWidget.setRowCount(1000)
+        tablerow = 0
+        for row in self.sqlquery:
+            self.tableWidget.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(row[0]))
+            self.tableWidget.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(row[1]))
+            self.tableWidget.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(row[2]))
+            self.tableWidget.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(str(row[3])))
+            self.tableWidget.setItem(tablerow, 4, QtWidgets.QTableWidgetItem(str(row[4])))
+            self.tableWidget.setItem(tablerow, 5, QtWidgets.QTableWidgetItem(row[5]))
+            tablerow += 1
+
+
+class query_ss:
+    def query_sss(self):
+        self.tableWidget.setRowCount(0)
+        for row_number, row_data in enumerate(self.sqlquery):
+            self.tableWidget.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+
+
+class MainWindow(QMainWindow, QWidget, query,query_ss,):
     def __init__(self):
-        super(MainWindow,self).__init__()
-        loadUi("main_assert.ui",self)
-        self.tableWidget.setColumnWidth(0, 200)
-        self.tableWidget.setColumnWidth(1, 205)
-        self.tableWidget.setColumnWidth(2, 125)
-        self.tableWidget.setColumnWidth(3, 205)
-        
+        super(MainWindow, self).__init__()
+        loadUi("main_assert.ui", self)
+        self.actionSony_2.triggered.connect(self.gotosony)
+        self.actionSprite.triggered.connect(self.gotosprite)
+        self.actionExit.triggered.connect(self.gotoexit)
+        self.serial_lineEdit.textChanged.connect(self.serial_no_search())
+        self.setWindowTitle('Assert Details')
+        self.data()
+        self.query_s()
+        widget.setFixedWidth(800)
+        widget.setFixedHeight(700)
+        widget.show
 
 
-app=QApplication(sys.argv)
-mainwindow=MainWindow()
-widget=QtWidgets.QStackedWidget()
+    def serial_no_search(self):
+
+        conn = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        cursor = conn.cursor()
+        searchol = ""
+        searchinput = self.serial_lineEdit.text()
+        searchol = self.searchinput.text()
+        search = ("SELECT * FROM Sheet1 WHERE [SERIAL NUMBER]= '+str(searchol)';")
+        data=cursor.execute(search,searchol)
+        row = searchol.fetchone()
+        model = QStandardItemModel(searchol, 6)
+
+        self.tableWidget.setRowCount(0)
+        for row_number, row_data in enumerate(searchol):
+            item=QStandardItem(row_data)
+            model.setItem(row,0,item)
+
+        self.filter_proxy_model = QSortFilterProxyModel()
+        self.filter_proxy_model.setSourceModel(model)
+        self.filter_proxy_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        self.filter_proxy_model.setFilterKeyColumn(0)
+
+
+
+        table = self.tableWidget()
+        table.setModel(self.filter_proxy_model)
+
+
+    def data(self):
+        conn = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        cursor = conn.cursor()
+        self.sqlquery = cursor.execute('SELECT * FROM Sheet1')
+
+    def gotosony(self):
+        Sony = sony()
+        widget.addWidget(Sony)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def gotosprite(self):
+        Sprite = sprite()
+        widget.addWidget(Sprite)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def gotoexit(self):
+        sys.exit()
+
+
+class sony(QDialog, query):
+    def __init__(self):
+        super(sony, self).__init__()
+        loadUi("add_sony_data.ui", self)
+        self.cancel.clicked.connect(self.gotomain)
+        self.loaddatas()
+        self.query_s()
+        widget.setFixedWidth(1000)
+        widget.setFixedHeight(700)
+        widget.show
+
+    def loaddatas(self):
+        conn = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        cursor = conn.cursor()
+        self.sqlquery = cursor.execute("SELECT * FROM Sheet1 WHERE Project='Sony' ")
+
+    def gotomain(self):
+        mainwindow = MainWindow()
+        widget.addWidget(mainwindow)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class sprite(QDialog, query_ss):
+    def __init__(self):
+        super(sprite, self).__init__()
+        loadUi("add_sprite_data.ui", self)
+        self.cancel.clicked.connect(self.gotomain)
+        self.loaddata_s()
+        self.query_sss()
+        widget.setFixedWidth(1000)
+        widget.setFixedHeight(700)
+        widget.show
+
+    def loaddata_s(self):
+        conn = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        cursor = conn.cursor()
+        self.sqlquery = cursor.execute("SELECT * FROM Sheet1 WHERE Project='Sprite' ")
+
+    def gotomain(self):
+        mainwindow = MainWindow()
+        widget.addWidget(mainwindow)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+app = QApplication(sys.argv)
+widget = QtWidgets.QStackedWidget()
+
+mainwindow = MainWindow()
 widget.addWidget(mainwindow)
-widget.setFixedHeight(700)
-widget.setFixedWidth(800)
 widget.show()
+
 try:
     sys.exit(app.exec_())
+
 except:
     print("Exiting")
