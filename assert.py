6504cbf6426b43ed429a61f2e129597aac8999a4
Clarence.B.Praison <71861162+clarencepraison@users.noreply.github.com>
2021-05-11 16:46:06 +0530

    Update assert.py

--- assert.py
+++ assert.py
@@ -2,13 +2,16 @@ import sys
 import pyodbc
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QMessageBox, QWidget, QTableWidgetItem, QLineEdit,QTableWidget, QHeaderView, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QMessageBox, QWidget, QTableWidgetItem,QLineEdit, QTableWidget,QHeaderView, QVBoxLayout
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtCore import Qt, QSortFilterProxyModel
 from PyQt5.QtCore import Qt
 from PyQt5.QtSql import *
 from PyQt5.QtSql import QSqlTableModel
 import re
+
+
+
 
 server = '10.209.25.201'
 database = 'AssertDB'
@@ -24,7 +27,6 @@ class query:
             for column_number, data in enumerate(row_data):
                 self.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
 
-
 class events:
     def add_data(self):
         self.SERIAL_NO = self.serial_lineEdit.text()
@@ -34,7 +36,7 @@ class events:
         self.PRICE = str(self.price_lineEdit.text())
         self.PROJECT = self.project_lineEdit.text()
         self.list = [self.SERIAL_NO, self.DESCRIPTION, self.VENDOR, self.DATE, self.PRICE, self.PROJECT]
-        x=re.findall("[a-z,A-Z]",self.SERIAL_NO or self.DESCRIPTION or self.VENDOR)
+        x = re.findall("[a-z,A-Z]", self.SERIAL_NO or self.DESCRIPTION or self.VENDOR)
         if not x:
             QMessageBox.about(self, "ERROR", "Enter any data")
         else:
@@ -43,40 +45,41 @@ class events:
             cursor = conn.cursor()
             sqlquery_ = '''INSERT INTO Sheet1 (SERIAL_NUMBER,DESCRIPTION,VENDOR,DATE,PRICE,Project) VALUES (?,?,?,?,?,?);'''
             QMessageBox.about(self, "SUCCESS", "Data Added Sucessfully!")
-            cursor.execute(sqlquery_,self.list)
+            cursor.execute(sqlquery_, self.list)
             conn.commit()
 
     def serial_search(self):
         conn = pyodbc.connect(
             'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
         cursor = conn.cursor()
-
-
         self.SERIAL_NO = self.serial_lineEdit.text()
         self.DESCRIPTION_NO = self.description_lineEdit.text()
         self.VENDOR_ = self.vendor_lineEdit.text()
         self.DATE_ = self.received_dateEdit.text()
         self.y = [self.SERIAL_NO, self.DESCRIPTION_NO, self.VENDOR_, self.DATE_]
         x = re.findall("[a-z,A-Z]", self.SERIAL_NO or self.DESCRIPTION_NO or self.VENDOR_ or self.DATE_)
-        if x :
-            command = '''SELECT * FROM Sheet1 WHERE SERIAL_NUMBER=? or DESCRIPTION=? or VENDOR=? or DATE=? '''
-            if self.y in command:
-                self.result = cursor.execute(command, self.y)
-
+        if x:
+            c = ("SELECT count(*) FROM Sheet1 WHERE SERIAL_NUMBER=? ")
+            r = cursor.execute(c, self.SERIAL_NO)
+            resul = cursor.fetchone()
+            print(resul)
+            ex = re.findall("[0-9]", resul)
+            print(ex)
+            if ex>1:
+                command = '''SELECT * FROM Sheet1 WHERE SERIAL_NUMBER=? or DESCRIPTION=? or VENDOR=? or DATE=? '''
+                result = cursor.execute(command, self.y)
                 self.tableWidget.setRowCount(0)
-
-                for row_number, row_data in enumerate(self.result):
+                for row_number, row_data in enumerate(result):
                     self.tableWidget.insertRow(row_number)
                     for column_number, data in enumerate(row_data):
                         self.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
                 conn.commit()
             else:
-                QMessageBox.about(self,"Error","No data Found")
+                QMessageBox.about(self, "Error", "No data Found")
         else:
-            QMessageBox.about(self,"Error","Enter Any DataFields")
+            QMessageBox.about(self, "Error", "Enter Any DataFields")
 
-
-def delete_data(self):
+    def delete_data(self):
         conn = pyodbc.connect(
             'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 
@@ -97,17 +100,16 @@ def delete_data(self):
         cursor.execute(command, SERIAL_NO)
         conn.commit()
 
-
 class Users(QMainWindow):
     def __init__(self):
-        super(Users, self).__init__()
-        loadUi("user_details.ui", self)
+        super(Users,self).__init__()
+        loadUi("user_details.ui",self)
         widget.setFixedWidth(950)
         widget.setFixedHeight(700)
         widget.show
 
 
-class MainWindow(QMainWindow, QWidget, query, events):
+class MainWindow(QMainWindow, QWidget, query,events):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("main_assert.ui", self)
@@ -124,9 +126,9 @@ class MainWindow(QMainWindow, QWidget, query, events):
         widget.setWindowTitle("IT Assert")
         widget.show
 
+
     def data(self):
-        conn = pyodbc.connect(
-            'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
         cursor = conn.cursor()
         self.sqlquery = cursor.execute('SELECT * FROM Sheet1')
 
@@ -162,8 +164,7 @@ class MainWindow(QMainWindow, QWidget, query, events):
             event.accept()
             print(exits)
 
-
-class sony(QMainWindow, query, events):
+class sony(QMainWindow, query,events):
     def __init__(self):
         super(sony, self).__init__()
         loadUi("add_sony_data.ui", self)
@@ -176,6 +177,7 @@ class sony(QMainWindow, query, events):
         self.homebutton.clicked.connect(self.gotomain_)
         self.loaddatas()
         self.query_s()
+
 
         widget.setFixedWidth(950)
         widget.setFixedHeight(700)
@@ -197,7 +199,7 @@ class sony(QMainWindow, query, events):
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
-class sprite(QMainWindow, query, events):
+class sprite(QMainWindow, query,events):
     def __init__(self):
         super(sprite, self).__init__()
         loadUi("add_sprite_data.ui", self)
@@ -228,6 +230,10 @@ class sprite(QMainWindow, query, events):
         mainwindow = MainWindow()
         widget.addWidget(mainwindow)
         widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+
+
 
 
 app = QApplication(sys.argv)
